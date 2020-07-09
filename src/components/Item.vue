b94d88e52f165093a62c8ea91e928106cd7781ac
fujisawatk <fuji19880707@gmail.com>
2020-07-09 20:18:28 +0900

    fix top page style
    コメント数表示追加、style調整。

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,11 @@
 <template>
-  <nb-list-item thumbnail :on-press="itemsPress">
-    <nb-left>
+  <nb-list-item
+    thumbnail
+    class="list-item"
+    :on-press="itemsPress"
+  >
+
+    <nb-left class="item-left">
       <nb-thumbnail
         square
         v-if="item.img != ''"
@@ -12,14 +17,44 @@
         :source="require('../../assets/icons/logo.png')"
       />
     </nb-left>
-    <nb-body>
-      <nb-text>{{ item.name }}</nb-Text>
-      <nb-text>{{ item.category }}</nb-Text>
+
+    <nb-body class="item-body">
+      <nb-text class="shop-category">
+        {{ item.category }}
+      </nb-text>
+      <nb-text class="item-title">
+        {{ item.name }}
+        </nb-text>
     </nb-body>
-    <nb-right>
-        <nb-icon type="AntDesign" name="like1" class="like-icon"/>
-        <nb-text>+{{ item.commentsCount }}</nb-text>
+
+    <nb-right class="item-right">
+      <nb-button
+        transparent
+        class="list-icon"
+      >
+        <nb-icon
+          type="AntDesign"
+          name="like1"
+          class="icon"
+        />
+        <nb-text class="icon-count">
+          +{{ item.commentsCount }}
+        </nb-text>
+      </nb-button>
+      <nb-button
+        transparent
+        class="list-icon"
+      >
+        <nb-icon
+          name="chatbubbles"
+          class="icon"
+        />
+        <nb-text class="icon-count">
+          +{{ item.commentsCount }}
+        </nb-text>
+      </nb-button>
     </nb-right>
+
   </nb-list-item>
 </template>
 
@@ -42,8 +77,34 @@ export default {
 </script>
 
 <style>
-.like-icon {
+.list-item {
+  flex: 1;
+}
+.item-left {
+  flex: 1;
+}
+.item-body {
+  flex: 3;
+}
+.shop-category {
+  font-size: 12px;
+  color: #888;
+}
+.item-right {
+  flex: 2;
+  flex-direction: row;
+}
+.list-icon {
+  position:relative;
+}
+.icon {
   color: #FF773E;
-  font-size: 35;
+  font-size: 20;
+}
+.icon-count {
+  position: absolute;
+  top: 15;
+  left: 25;
+  color: #444;
 }
 </style>
